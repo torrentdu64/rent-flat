c84d781c715c72de95f88fc8dabfbc31cf53b7bb
ben <torrentdu64@gmail.com>
2021-12-13 12:29:39 +1300

    View implementation for display flats and flat details such room

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :flats
 end
