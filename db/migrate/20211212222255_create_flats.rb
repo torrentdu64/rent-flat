21fe993b9176c9596d2090cc10ea3931470dd681
ben <torrentdu64@gmail.com>
2021-12-13 11:29:16 +1300

    flat room rent association generate and migrate

--- db/migrate/20211212222255_create_flats.rb
+++ db/migrate/20211212222255_create_flats.rb
@@ -2,7 +2,6 @@ class CreateFlats < ActiveRecord::Migration[6.1]
   def change
     create_table :flats do |t|
       t.string :title
-      t.references :room, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_220655) do
+ActiveRecord::Schema.define(version: 2021_12_12_222409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "flats", force: :cascade do |t|
     t.string "title"
-    t.bigint "room_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_flats_on_room_id"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
@@ -37,8 +35,10 @@ ActiveRecord::Schema.define(version: 2021_12_12_220655) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
+    t.bigint "flat_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_rooms_on_flat_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(version: 2021_12_12_220655) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "flats", "rooms"
   add_foreign_key "flats", "users"
   add_foreign_key "rents", "flats"
   add_foreign_key "rents", "users"
+  add_foreign_key "rooms", "flats"
 end
