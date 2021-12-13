f5af770c6d53e7c8dc2bd219282992eca97bbf9d
ben <torrentdu64@gmail.com>
2021-12-13 17:01:03 +1300

    room assosiation for flat present

--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -3,13 +3,17 @@ require "application_system_test_case"
 class FlatsTest < ApplicationSystemTestCase
   test "flat index system" do
     visit "/flats"
-
     assert_selector "h1", text: "Flats#index"
   end
 
-  test "flat name present" do
+  test "flat index name present" do
     visit flats_path
-
     assert_selector "a", text: "Ponsonby flat"
   end
+
+  test "flat show page display" do
+    visit "/flats/1"
+    assert_selector "li", text: "kitchen"
+  end
+
 end
